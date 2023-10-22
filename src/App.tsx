d648c6a917f797c63c4db231720a09111c2f9413
mparchin <mmzparchin@gmail.com>
2023-10-22 23:40:51 +0330

    config

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { ThemeMode, useThemeStore } from "./theme";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
-import { IconButton } from "@mui/material";
 import SearchAppBar from "./components/SearchAppBar";
 import SpellList from "./components/SpellList";
 import SpellDetailDialog from "./components/SpellDetailDialog";
 
 export default function App() {
-  const mode = useThemeStore((state) => state.mode);
   return (
     <>
       <div className="flex-col flex w-screen h-screen max-h-screen overflow-hidden">
--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,9 @@
 import { Spell } from "./models/spell";
 import axios from "axios";
 
-const apiAddress = "http://localhost";
+const apiAddress = import.meta.env.VITE_API_ADDRESS
+  ? import.meta.env.VITE_API_ADDRESS
+  : "http://localhost";
 
 export const getSpells = (url: string) =>
   axios.get<Spell[]>(`${apiAddress}${url}`).then((res) => res.data);
