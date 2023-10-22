6f7da327a114af1d580522a835ef8438e8c08aae
mparchin <mmzparchin@gmail.com>
2023-10-22 23:54:54 +0330

    api url

--- src/api.tsx
+++ src/api.tsx
@@ -1,9 +1,7 @@
 import { Spell } from "./models/spell";
 import axios from "axios";
 
-const apiAddress = import.meta.env.VITE_API_ADDRESS
-  ? import.meta.env.VITE_API_ADDRESS
-  : "http://localhost";
+const apiAddress = "http://eldoriantales.com:3000";
 
 export const getSpells = (url: string) =>
   axios.get<Spell[]>(`${apiAddress}${url}`).then((res) => res.data);
