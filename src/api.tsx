bda34510dee50e2de6e549e235722a80f467397c
mparchin <mmzparchin@gmail.com>
2023-10-22 23:56:41 +0330

    api address

--- src/api.tsx
+++ src/api.tsx
@@ -1,7 +1,7 @@
 import { Spell } from "./models/spell";
 import axios from "axios";
 
-const apiAddress = "http://eldoriantales.com:3000";
+const apiAddress = "http://eldoriantales.com:8000";
 
 export const getSpells = (url: string) =>
   axios.get<Spell[]>(`${apiAddress}${url}`).then((res) => res.data);
