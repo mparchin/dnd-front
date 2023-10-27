158293949bff5c715c256fbaf9ef820d1b190c52
mparchin <mmzparchin@gmail.com>
2023-10-27 12:02:56 +0330

    build scripts

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+IMAGE="mparchin/dnd-front"
+ADDRESS="dnd-front"
+if [ "$1" = "production" ]
+  then
+    BRANCH=$1
+    VITE_API_ADDRESS="https://backend.eldoriantales.com"
+  else
+    BRANCH="development"
+    VITE_API_ADDRESS="http://localhost"
+fi
+SERVER="91.107.242.150"
+ssh $SERVER "cd $ADDRESS && \
+git switch $BRANCH && \
+git pull && \
+docker buildx build -t $IMAGE:$BRANCH --push --build-args VITE_API_ADDRESS=$VITE_API_ADDRESS ."
